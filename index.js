ab0f821b30233d753bc55b2cab9b78ff33a4eba0
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-02-01 11:59:23 +0700

    Finished the task

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.use(express.static("public"))
+app.use(express.static("public")); 
 
 app.get("/", (req, res) => {
   res.render("index.ejs")
@@ -23,3 +23,9 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
